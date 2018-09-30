e3aae4c442965d5b5a1de3e56125de031ed603ce
Dilemma <wu_yi827@126.com>
2018-09-30 15:55:29 -0400

    生成迷宫

---
+++ createmaze.py
@@ -0,0 +1,22 @@
+
+import random
+
+def createmaze(n,p):
+    valuelist=[]
+    for i in range(n*n):
+        b=random.uniform(0,1)
+        if b<=p:
+            valuelist.append(1)
+        else:
+            valuelist.append(0)
+    k=0
+    maze = [([0] * n) for p in range(n)]
+    for i in range(n):
+        for j in range(n):
+            maze[i][j] = valuelist[k]
+            k += 1
+    maze[0][0]=0
+    maze[n-1][n-1]=0
+    return maze
+
+   
