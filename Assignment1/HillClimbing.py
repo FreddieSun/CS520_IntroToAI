e1924cadb6c91f8afd7df8558d6db0d90ddb754d
FreddieSun <freddiesun95@gmail.com>
2018-10-06 15:39:45 -0400

    爬山算法框架

--- Assignment1/HillClimbing.py
+++ Assignment1/HillClimbing.py
@@ -5,11 +5,34 @@ class HillClimbing:
         self.mazeSize = mazeSize
         self.maze = []
         self.p = p
+        self.finalSets = []
 
-    def HillClimbing(self):
-        while(True)
+    def hillClimbing(self):
+        exitFlag = False
+        # 对每一个maze，找到他的最优解， 故循环100次
+        for i in range(100):
+            while(exitFlag):
+                # 对当前的maze，爬山找到最优解
+                next = self.randomWalk(self.initSets[i])
+                if (self.evaluateMaze(next) > self.evaluateMaze(self.initSets[i])):
+                    # 改变后的maze赋给当前的maze，进入下一次迭代
+                    self.initSets[i] = next
+
+            self.finalSets[i] = self.initSets[i]
+
+        # 比较finalSets中的所有解，找到全局最优解
 
 
+    def randomMaze(self):
+        print('randomMaze')
+
+
+    def randomWalk(self, maze):
+        print('randomWalk')
+
+    def evaluateMaze(self, maze):
+        print('evaluateMaze')
+        return 1
 
 if __name__ == '__main__':
     print('main function')
