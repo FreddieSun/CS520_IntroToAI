a30b4ab654e04df29ec29e9427dfa3ba7beb08d9
FreddieSun <freddiesun95@gmail.com>
2018-10-06 15:26:43 -0400

    HillClimbing

--- Assignment1/HillClimbing.py
+++ Assignment1/HillClimbing.py
@@ -1,7 +1,17 @@
 class HillClimbing:
-    def __init__(self, N, P):
-        self.N = N
+    def __init__(self, initSetsNum, mazeSize, p):
+        self.initSetsNum = initSetsNum
+        self.initSets = []
+        self.mazeSize = mazeSize
+        self.maze = []
+        self.p = p
+
+    def HillClimbing(self):
+        while(True)
+
 
 
 if __name__ == '__main__':
     print('main function')
+    hillClimbing = HillClimbing(100, 100, 0.2)
+
