3f815b4b81a375ddaa9169b30fd12763606b79b3
FreddieSun <freddiesun95@gmail.com>
2018-10-16 15:39:57 -0400

    进入game方法后，先随机地点一个不是雷的点来触发算法。

--- Assignment2/MineSweeper.py
+++ Assignment2/MineSweeper.py
@@ -1,4 +1,6 @@
 import Grid
+import sys
+import random
 from Assignment2.Grid import *
 
 
@@ -10,22 +12,6 @@ class MineSweeper:
         self.totalNumOfMine = self.grid.numOfMine
         self.currentNumOfMine = self.totalNumOfMine
 
-    def flagMines(self, grid):
-        flag = False
-        for i in range(grid.height):
-            for j in range(grid.width):
-                cell = grid.getCell(i, j)
-                if not cell.isCovered:
-                    numOfmines = cell.numOfMines
-                    if numOfmines >= 1 and numOfmines == grid.numOfCoveredCell(i, j):
-                        flag = True
-                        for ii in range(-1, 2):
-                            for jj in range(-1, 2):
-                                adj = grid.getCell(i + ii, j + jj)
-                                if not adj.isOutside and adj.isCovered:
-                                    adj.isFlag = True
-        return flag
-
     def drawGrid(self, grid):
         for i in range(grid.height):
             for j in range(grid.width):
@@ -33,7 +19,7 @@ class MineSweeper:
                 if curCell.isMine:
                     print('X ', end='')
                 else:
-                    print('O ', end='')
+                    print(curCell.numOfMines, '', end='')
             print('\n')
 
     def clickCell(self, grid):
@@ -61,18 +47,55 @@ class MineSweeper:
                                 if adj.isMine:
                                     isLose = True
                                     adj.isCovered = False
+                                    self.currentNumOfMine -= 1
                                     return [successClick, isLose]
                                 adj.isCovered = False
                                 self.currentNumOfMine -= 1
 
-                return [successClick, isLose]
+        return [successClick, isLose]
+
+    def flagMines(self, grid):
+        flag = False
+        for i in range(grid.height):
+            for j in range(grid.width):
+                cell = grid.getCell(i, j)
+                if not cell.isCovered:
+                    numOfmines = cell.numOfMines
+                    if numOfmines >= 1 and numOfmines == grid.numOfCoveredCell(i, j):
+                        flag = True
+                        for ii in range(-1, 2):
+                            for jj in range(-1, 2):
+                                adj = grid.getCell(i + ii, j + jj)
+                                if not adj.isOutside and adj.isCovered:
+                                    adj.isFlag = True
+        return flag
+
 
     def game(self):
         print('Start')
         self.drawGrid(self.grid)
+
+        # random choose a cell which is not bomb
+        # in order to trigger the algorithm
+        firstTrigger = True
+        while firstTrigger:
+            i = random.randint(0,self.grid.height)
+            j = random.randint(0, self.grid.width)
+            if not self.grid.getCell(i, j).isMine:
+                # click one cell and trigger the algorithm
+                self.grid.getCell(i, j).isCovered = False
+                firstTrigger = False
+
         while not self.currentNumOfMine == 0:
-            while self.clickCell(self.grid)[1] and self.clickCell(self.grid)[0]:
-                pass
+            for i in range(sys.maxsize):
+                successClick, isLose = self.clickCell(self.grid)
+                if isLose:
+                    print('Game Over')
+                    sys.exit()
+                if successClick:
+                    continue
+                else:
+                    break
             while self.flagMines(self.grid):
                 pass
         # self.drawGrid(self.grid)
