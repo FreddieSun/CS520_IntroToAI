a99c8daca902a83e58b1ef78dd6c4262d436fe8e
FreddieSun <freddiesun95@gmail.com>
2018-10-15 22:10:29 -0400

    增加isFlag

--- Assignment2/MineSweeper.py
+++ Assignment2/MineSweeper.py
@@ -1,16 +1,17 @@
 import Grid
-from Assignment2.Cell import *
+from Assignment2.Grid import *
 
 
 class MineSweeper:
     def __init__(self):
+        grid = Grid(9, 9, 0.1)
         print(1)
 
     def markMines(self,grid):
         for i in range(grid.height):
             for j in range(grid.width):
-                numOfmines = grid.getCell(i,j).numOfMines
-                if numOfmines >= 1 and numOfmines == grid.numOfCoveredCell(i,j):
+                numOfMines = grid.getCell(i,j).numOfMines
+                if numOfMines >= 1 and numOfMines == grid.numOfCoveredCell(i,j):
                     for ii in range(-1, 2):
                         for jj in range(-1, 2):
                             adj = grid.getCell(i + ii, j + jj)
@@ -19,6 +20,31 @@ class MineSweeper:
 
 
 
+
+
+
+    def clickCell(self, grid):
+        flag = False
+
+        for i in range(grid.borderHeight):
+            for j in range(grid.borderWidth):
+
+                curCell = grid.getCell(i, j)
+
+                // if
+                if not curCell.isCovered:
+                    continue
+
+                numOfMines = curCell.numOfMines
+                numOfActualMines = grid.numOfFlags(i, j)
+                numOfCoveredCell = grid.numOfCoveredCell(i, j)
+
+                if numOfMines == numOfActualMines and numOfCoveredCell > 0:
+                    for ii in range(-1, 2):
+                        for jj in range(-1, 2):
+                            adj = grid.getCell(i + ii, j + jj)
+                            if not adj.isOutside and adj.isCovered:
+                                 adj.isCovered = False
 
 
 
