76dea975659d84ad5425d67c1d221cd2be6263bc
XinyuLyu <xl422@scarletmail.rutgers.edu>
2018-10-30 16:17:07 -0400

    print Grid finished

--- Assignment3/Grid.py
+++ Assignment3/Grid.py
@@ -3,33 +3,45 @@ from Assignment3.Cell import *
 
 
 class Grid:
-    def __init__(self, height, width):
-        self.height = 50
-        self.width = 50
+    def __init__(self, N):
+        self.N = N #50
         self.grid = []
         self.flatP = 0.2
         self.hillP = 0.3
         self.forestP = 0.3
         self.caveP = 0.2
+        self.numofFlat = 0
+        self.numofHill = 0
+        self.numofForest = 0
+        self.numofCave= 0
+        self.generateGrid()
+
+
 
 
     def generateGrid(self):
-        for i in range(self.height):
-            for j in range(self.width):
+        for i in range(self.N):
+            for j in range(self.N):
                 rNum = random.random()
                 # [0.0,0.2)
                 if 0 <= rNum < self.flatP:
                     cell = Cell(1)
                     self.grid.append(cell)
+                    self.numofFlat += 1
                 # [0.2,0.5)
                 elif self.flatP <= rNum < self.flatP+self.hillP:
                     cell = Cell(2)
                     self.grid.append(cell)
+                    self.numofHill += 1
                 # [0.5,0.8)
                 elif self.flatP+self.hillP <= rNum < self.flatP+self.hillP+self.forestP:
                     cell = Cell(3)
                     self.grid.append(cell)
+                    self.numofForest += 1
                 # [0.8,1.0)
                 else:
                     cell = Cell(4)
                     self.grid.append(cell)
+                    self.numofCave += 1
+    def getCell(self, x, y):
+        return self.grid[x * self.N + y]
---
+++ Assignment3/PrintGrid.py
@@ -0,0 +1,78 @@
+import pygame
+from Assignment3.Cell import *
+from Assignment3.Grid import *
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREY = (192, 192, 192)
+DARKGREY = (41,36,33)
+GREEN = (61,145,64)
+
+size = 20
+gap = 2
+
+HEIGHT = 10
+WIDTH = 20
+minP = 0.2
+
+
+
+def gridList(grid):
+    smallGrid = []
+    for i in range(grid.N):
+        for j in range(grid.N):
+            smallGrid.append(grid.getCell(i, j))
+
+    GridList = []
+    for i in range(len(smallGrid)):
+        if smallGrid[i].terrain == 1:
+            GridList.append(1)
+        elif smallGrid[i].terrain == 2:
+            GridList.append(2)
+        elif smallGrid[i].terrain == 3:
+            GridList.append(3)
+        elif smallGrid[i].terrain == 4:
+            GridList.append(4)
+        else:
+            GridList.append(smallGrid[i].numOfMines)
+    return GridList
+
+
+def drawInitialGrid(gridlist, gridHeight, gridWidth):
+    pygame.init()
+    width = size * gridWidth + (gridWidth + 1) * gap
+    height = size * gridHeight + (gridHeight + 1) * gap
+    surface = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("Maps")
+    exitflag = True
+    while exitflag:
+        surface.fill(BLACK)
+        for i in range(gridHeight):
+            for j in range(gridWidth):
+                if gridlist[i * gridWidth + j] == 1:
+                    pygame.draw.rect(surface, WHITE, [(gap + size) * j + gap, (gap + size) * i + gap, size, size])
+                elif gridlist[i * gridWidth + j] == 2:
+                    pygame.draw.rect(surface, DARKGREY, [(gap + size) * j + gap, (gap + size) * i + gap, size, size])
+                elif gridlist[i * gridWidth + j] == 3:
+                    pygame.draw.rect(surface, GREEN, [(gap + size) * j + gap, (gap + size) * i + gap, size, size])
+                elif gridlist[i * gridWidth + j] == 4:
+                    pygame.draw.rect(surface, DARKGREY, [(gap + size) * j + gap, (gap + size) * i + gap, size, size])
+        pygame.display.flip()
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exitflag = False
+
+if __name__ == '__main__':
+    grid = Grid(100)
+    GridList = gridList(grid)
+    print("flat ", grid.numofFlat)
+    print("hill ", grid.numofHill)
+    print("forest ", grid.numofForest)
+    print("cave", grid.numofCave)
+    drawInitialGrid(GridList,grid.N,grid.N)
+
+
+
