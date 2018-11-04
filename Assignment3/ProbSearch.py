7b4c6de8c26af87683d293e9e0840fcc8d0144ce
FreddieSun <freddiesun95@gmail.com>
2018-11-03 21:00:22 -0400

    init ProbSearch

---
+++ Assignment3/ProbSearch.py
@@ -0,0 +1,20 @@
+class ProbSearch:
+
+    def __init__(self):
+        print('init method')
+
+    def probSearch(self):
+        print('probSearch method')
+
+    def findTarget(self, cell):
+        print('findTarget method')
+
+    def updateProb(self, i, j):
+        print('updateProb method')
+
+    def searchCell(self):
+        print('searchCell method')
+
+
+if __name__ == '__main__':
+    print('main method')
