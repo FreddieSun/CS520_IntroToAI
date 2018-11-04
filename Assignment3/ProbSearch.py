b915dcc7115e67739a669e2c3c13941b0ed6db43
XinyuLyu <xl422@scarletmail.rutgers.edu>
2018-11-03 21:29:38 -0400

    prob update

--- Assignment3/ProbSearch.py
+++ Assignment3/ProbSearch.py
@@ -9,10 +9,24 @@ class ProbSearch:
     def findTarget(self, cell):
         print('findTarget method')
 
-    def updateProb(self, i, j):
+    def updateProb(self, i, j, grid):
+        searchedCell = grid.getCell(i,j)
+        Pj = searchedCell.Pr1
+        Tj = 1-searchedCell.Pf
+        grid.getCell(i, j).Pr1 = Pj * Tj
+        for ii in grid.N:
+            for jj in grid.N:
+                OtherCell = grid.getCell(ii,jj)
+                if i == ii and j == jj:
+                    continue
+                else:
+                    Pi = OtherCell.Pr1
+                    grid.getCell(ii, jj).Pr1 = Pi * (1 + Pj * (1-Tj) / (1-Pj))
+        return grid
         print('updateProb method')
 
     def searchCell(self):
+
         print('searchCell method')
 
 
