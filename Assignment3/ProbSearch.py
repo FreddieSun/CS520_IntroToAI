012268d38ab0197aa3d742f0eb66148d40b670ed
FreddieSun <freddiesun95@gmail.com>
2018-11-03 21:11:40 -0400

    update probSearch

--- Assignment3/ProbSearch.py
+++ Assignment3/ProbSearch.py
@@ -1,19 +1,32 @@
+import random
+from Assignment3.Grid import *
+from Assignment3.PrintGrid import *
+
 class ProbSearch:
 
     def __init__(self):
         print('init method')
-
-    def probSearch(self):
-        print('probSearch method')
+        self.grid = Grid()
+        self.numOfSearches = 0
+        grid.generateGrid()
 
     def findTarget(self, cell):
         print('findTarget method')
 
-    def updateProb(self, i, j):
+    def updateProb(self, cell):
         print('updateProb method')
 
-    def searchCell(self):
+    def searchCell(self, grid, type):
         print('searchCell method')
+
+    def probSearch(self):
+        print('probSearch method')
+
+        while True:
+            self.numOfSearches += 1
+            print(self.numOfSearches,'th search')
+            cell = self.searchCell()
+
 
 
 if __name__ == '__main__':
