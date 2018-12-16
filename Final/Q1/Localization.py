63f9904b9d192a0f488a30f8083490f5bb5874b2
XinyuLyu <xl422@scarletmail.rutgers.edu>
2018-12-16 17:45:55 -0500

    eee

--- Final/Q1/Localization.py
+++ Final/Q1/Localization.py
@@ -25,8 +25,40 @@ def getblocksaround(x,y,maze):
             if temp == 1:
                 number += 1
     return number
+'''
+find the cell with 5 blocks around
+'''
+def get_5blocks_cell(maze):
+    cell = []
+    for x in range(len(maze)):
+        for y in range(len(maze[0])):
+            if getblocksaround(x, y, maze) ==5:
+                cell.append([x,y])
+    return cell
+'''
+move the cells 
+actions =['U','D','L','R']
+'''
+def move(maze,cells, actions):
+    for action in actions:
+        if action == 'U':
+            for cell in cells:
+                if maze[cell[0]-1][cell[1]] == 0:
+                    cell[0]-=1
+        elif action == 'D':
+            for cell in cells:
+                if maze[cell[0]+1][cell[1]] == 0:
+                    cell[0]+=1
+        elif action == 'L':
+            for cell in cells:
+                if maze[cell[0]][cell[1]-1] == 0:
+                    cell[1]-=1
+        elif action == 'R':
+            for cell in cells:
+                if maze[cell[0]][cell[1]+1] == 0:
+                    cell[1]+=1
+    return cells
 
 if __name__ == '__main__':
-    data = pd.read_table("Maze.txt",header=None)
+    data = pd.read_table("Maze.txt", header=None)
     maze = generateMaze(data)
-
