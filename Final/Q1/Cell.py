77d70f2bbb4cb5bd5f1e19a1bbd74930b5775644
XinyuLyu <xl422@scarletmail.rutgers.edu>
2018-12-16 17:02:36 -0500

    eee

--- Final/Q1/Cell.py
+++ Final/Q1/Cell.py
@@ -1,12 +1,7 @@
 class Cell:
 
     def __init__(self, isMine):
-        #self.isMine = isMine
-        #self.isWrong = False
-        #self.isOutside = False
-        #self.isCovered = True
-        #self.numOfMines = 0
-        #self.isFlag = False
+
 
 
 
