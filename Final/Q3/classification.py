7522704a9b35523c5b30449077d91181388ccbf0
XinyuLyu <xl422@scarletmail.rutgers.edu>
2018-12-17 12:24:36 -0500

    eee

--- Final/Q3/classification.py
+++ Final/Q3/classification.py
@@ -1,8 +1,10 @@
 from sklearn import svm, tree
-
+from sklearn.svm import LinearSVC
+from sklearn.ensemble import RandomForestClassifier
 import tensorflow as tf
 from sklearn.datasets import load_digits
 from sklearn.cross_validation import train_test_split
+from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import LabelBinarizer
 
 A1 = [255, 0, 0, 255, 255, 255, 255, 255, 0, 0, 255, 255, 255, 0, 0, 255, 0, 0, 255, 255, 0, 0, 0, 0, 255]
@@ -11,10 +13,10 @@ A3 = [255, 255, 0, 0, 255, 255, 255, 255, 255, 255, 255, 0, 0, 0, 0, 0, 0, 0, 25
 A4 = [0, 0, 255, 255, 255, 255, 0, 0, 0, 0, 255, 255, 255, 0, 0, 255, 255, 0, 0, 255, 255, 0, 0, 0, 0]
 A5 = [0, 0, 0, 255, 255, 0, 0, 0, 0, 0, 255, 255, 255, 255, 255, 255, 0, 0, 255, 255, 255, 255, 255, 255, 255]
 
-Train_A_data = [A1, A2, A3, A4, A5]
-for A in Train_A_data:
-    for i in range(5):
 
+A11 = [255,255,0,0,255,0,0,255,255,255,0,0,255,255,255,255]
+
+Train_A_data = [A1, A2, A3, A4, A5]
 
 Train_A_tag = [0, 0, 0, 0, 0]
 
@@ -38,10 +40,18 @@ M3 = [0, 0, 255, 255, 255, 255, 255, 255, 0, 255, 0, 0, 0, 0, 255, 255, 255, 255
 M4 = [255, 0, 0, 0, 255, 255, 0, 0, 0, 255, 0, 0, 255, 255, 255, 255, 255, 0, 0, 255, 255, 255, 255, 0, 0]
 M5 = [0, 255, 255, 255, 255, 0, 255, 0, 0, 255, 255, 255, 255, 255, 0, 255, 0, 0, 255, 0, 255, 255, 255, 255, 255]
 Mystery = [M1, M2, M3, M4, M5]
-
-# clf = svm.SVC()  # class
-# clf.fit(Train, Tag)  # training the svc model
-model = tree.DecisionTreeClassifier()
+'''
+model = svm.SVC(C=0.1, cache_size=200, class_weight=None, coef0=0.0,
+ decision_function_shape='ovr', degree=3, gamma='auto', kernel='rbf',
+ max_iter=-1, probability=False, random_state=None, shrinking=True,
+ tol=0.001, verbose=False)  # class
+ '''
+model = svm.LinearSVC(penalty='l2', loss='squared_hinge', dual=True,
+                       tol=1e-4)
+#model = tree.DecisionTreeClassifier()
+#model = MLPClassifier()
+#model = RandomForestClassifier()
 model.fit(X, y)
-result = model.predict(Mystery)  # predict the target of testing samples
+result = model.predict(Mystery)
 print(result)
+
