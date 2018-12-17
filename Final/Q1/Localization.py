c574fe97bcce1e9337c22149a38998432f57df59
FreddieSun <freddiesun95@gmail.com>
2018-12-16 19:34:21 -0500

    Merge branch 'master' of /Users/weijiasun/Documents/GitHub/ECE519/CS520_IntroToAI with conflicts.

--- Final/Q1/Localization.py
+++ Final/Q1/Localization.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
+
 def generateMaze(data):
-    maze=[]
+    maze = []
     for i in range(43):
         temp = []
         for j in data.loc[i]:
@@ -14,12 +15,13 @@ def generateMaze(data):
         maze.append(temp)
     return maze
 
-def getBlocksAround(x,y,maze):
+
+def getBlocksAround(x, y, maze):
     number = 0
     if x == 0 or y == 0 or x == 42 or y == 56:
         return 0
-    for i in range(-1,2):
-        for j in range(-1,2):
+    for i in range(-1, 2):
+        for j in range(-1, 2):
             if i == 0 and j == 0:
                 continue
             temp = maze[x + i][y + j]
@@ -27,81 +29,90 @@ def getBlocksAround(x,y,maze):
                 number += 1
     return number
 
-def get_blocks_cell(maze,observation):
+
+'''
+find the cell with 5 blocks around
+'''
+
+
+def get_blocks_cell(maze, observation):
     cells = []
     for x in range(len(maze)):
         for y in range(len(maze[0])):
             if getBlocksAround(x, y, maze) == observation:
-                cells.append([x,y])
+                cells.append([x, y])
     return cells
+
+
 '''
 move the cells 
 actions =['U','D','L','R']
 '''
-def move(maze,cells, action):
+
+
+def move(maze, cells, action):
     if action == 'U':
         for cell in cells:
-            if maze[cell[0]-1][cell[1]] == 0:
-                cell[0]-=1
+            if maze[cell[0] - 1][cell[1]] == 0:
+                cell[0] -= 1
     elif action == 'D':
         for cell in cells:
-            if maze[cell[0]+1][cell[1]] == 0:
-                cell[0]+=1
+            if maze[cell[0] + 1][cell[1]] == 0:
+                cell[0] += 1
     elif action == 'L':
         for cell in cells:
-            if maze[cell[0]][cell[1]-1] == 0:
-                cell[1]-=1
+            if maze[cell[0]][cell[1] - 1] == 0:
+                cell[1] -= 1
     elif action == 'R':
         for cell in cells:
-            if maze[cell[0]][cell[1]+1] == 0:
-                cell[1]+=1
+            if maze[cell[0]][cell[1] + 1] == 0:
+                cell[1] += 1
     return cells
 
-def update(maze,cells,action,obervation):
-    nextcells = move(maze,cells,action)
+
+def update(maze, cells, action, obervation):
+    nextcells = move(maze, cells, action)
     returncell = []
     for cell in nextcells:
-        if getBlocksAround(cell[0],cell[1],maze) == obervation:
+        if getBlocksAround(cell[0], cell[1], maze) == obervation:
             returncell.append(cell)
     return returncell
 
-def findMostProbability(cells):
-    start = 0
-    end = 1
-    maxtime = 1
-    finalresult = []
-    while end < len(cells) :
-        if cells[start] != cells[end] and cells[start] == cells[start + 1] :
-            start = end - 1
 
-        elif cells[start] != cells[end] and cells[start] != cells[start + 1] :
-            start += 1
-            end += 1
-        elif cells[start] == cells[end]:
+def findMostProbability1(cells):
+    info = {}
+    info = dict()
+    for i in range(len(cells)):
+        (ii, jj) = cells[i]
+        key = (ii, jj)
+        if info.__contains__(key):
+            info[key] = info.get(key) + 1
+        else:
+            info[key] = 1
 
-            if end - start + 1 > maxtime:
-                maxtime = end - start + 1
-            end += 1
-    for i in range(len(cells) - maxtime):
-        if cells[i] == cells[i + maxtime - 1]:
-            finalresult.append(cells[i])
-    totalnumber = len(cells)
-    probability = maxtime / totalnumber
-    print(probability)
-    return finalresult
+    max_key = cells[0]
+    max_value = 0
+    for key, value in info.items():
+        if value > max_value:
+            max_key = key
+            max_value = value
 
+    result = []
+    for key, value in info.items():
+        if value == max_value:
+            result.append(key)
 
+    return result
 
 
 if __name__ == '__main__':
     data = pd.read_table("Maze.txt", header=None)
     maze = generateMaze(data)
-    observations = [5,5,5]
-    actions = ['L','L']
-    cells = get_blocks_cell(maze,observations[0])
+    observations = [5, 5, 5]
+    actions = ['L', 'L']
+    cells = get_blocks_cell(maze, observations[0])
     n = 0
-    while n < len(actions) :
-        cells = update(maze,cells,actions[n],observations[n+1])
+    while n < len(actions):
+        cells = update(maze, cells, actions[n], observations[n + 1])
         n += 1
-    print(cells)
     print(findMostProbability(cells))
