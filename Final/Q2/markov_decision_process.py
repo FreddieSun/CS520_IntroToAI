20da9fe33f458e4d89fe9c8d4658c5b69f3b6536
tangxun1688 <tangxun1688@gmail.com>
2018-12-18 23:31:53 -0500

    modify

---
+++ Final/Q2/markov_decision_process.py
@@ -0,0 +1,88 @@
+"""
+Inputs, global variables
+"""
+
+'''0 - start, 1~8 - used, 9 - dead'''
+utility = []
+for i in range(10):
+    utility.append(0)
+
+'''probabilities of transfering to next state'''
+transfer_probability = []
+for i in range(10):
+    transfer_probability.append(0.1 * i)
+transfer_probability[0] = 1
+
+'''probabilities of staying in current state'''
+stay_probability = []
+for i in transfer_probability:
+    stay_probability.append(1 - i)
+
+'''probabilities of replacing'''
+replace_probability = []
+for i in range(10):
+    replace_probability.append(1)
+replace_probability[0] = 0
+
+'''reward of transfer to next state'''
+reward = []
+for i in range(10):
+    reward.append(100 - i * 10)
+reward[-1] = 0
+
+'''cost of replacing to new'''
+cost_replace = 250
+
+'''discount factor'''
+beta = 0.9
+
+'''the maximum error allowed in the utility of any state'''
+epsilon = 0.001
+
+'''iteration times'''
+epochs = 0
+
+'''optimal policy and two actions'''
+optimal_policy = []
+action_choice = ['use', 'replace']
+
+if __name__ == '__main__':
+    # value iterate
+    while True:
+        utility_update = []
+        actions = []
+
+        '''the maximum change in the utility of any state in an iteration'''
+        delta = 0
+        epochs += 1
+
+        '''update utility of new and used states'''
+        for i in range(9):
+            value = [(stay_probability[i] * (reward[i] + beta * utility[i]) + transfer_probability[i] * (
+                    reward[i] + beta * utility[i + 1])), replace_probability[i] * (-cost_replace + beta * utility[0])]
+
+            u_update = max(value)
+            actions.append(value.index(u_update))
+            utility_update.append(u_update)
+            if abs(u_update - utility[i]) > delta:
+                delta = abs(u_update - utility[i])
+
+        '''update utility of dead state'''
+        u_update = -cost_replace + beta * utility[0]
+        utility_update.append(u_update)
+        actions.append(1)
+
+        if abs(u_update - utility[-1]) > delta:
+            delta = abs(u_update - utility[-1])
+
+        utility = utility_update
+        optimal_policy = actions
+
+        '''terminate'''
+        if delta < epsilon * (1 - beta) / beta:
+            break
+
+    optimal_policy = [action_choice[i] for i in optimal_policy]
+    print(utility)
+    print(optimal_policy)
+    print('epochs = ', epochs)
---
+++ Final/Q3/Q3.py
@@ -0,0 +1,128 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler
+import warnings
+warnings.filterwarnings(action='ignore', category=DeprecationWarning)
+# train dataset
+A1 = [1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1]
+A2 = [1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0]
+A3 = [0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0]
+A4 = [1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1]
+A5 = [1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1]
+A6 = [1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0]
+A7 = [0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0]
+A8 = [1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1]
+A9 = [0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1]
+A10 = [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1]
+A11 = [0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0]
+A12 = [1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1]
+A13 = [0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1]
+A14 = [1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1]
+A15 = [0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1]
+A16 = [1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0]
+A17 = [0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0]
+A18 = [1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1]
+A19 = [0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0]
+A20 = [1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1]
+A21 = [1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0]
+A22 = [1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1]
+A23 = [1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0]
+A24 = [1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1]
+A25 = [1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1]
+
+B1 = [1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1]
+B2 = [1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1]
+B3 = [1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0]
+B4 = [1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1]
+B5 = [1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1]
+B6 = [0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1]
+B7 = [1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0]
+B8 = [0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0]
+B9 = [1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0]
+B10 = [0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1]
+B11 = [1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1]
+B12 = [1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1]
+B13 = [1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0]
+B14 = [0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0]
+B15 = [1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0]
+B16 = [0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1]
+B17 = [0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1]
+B18 = [1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1]
+B19 = [0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1]
+B20 = [1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1]
+B21 = [1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1]
+B22 = [1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1]
+B23 = [0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1]
+B24 = [0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0]
+B25 = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1]
+
+X = [A1, A2, A3, A4, A5, A6, A7, A8, A9, A10,A11,A12,A13,A14,A15,A16,A17,A18,A19,A20,A21,A22,A23,A24,A25,
+     B1, B2, B3, B4, B5, B6, B7, B8, B9, B10,B11,B12,B13,B14,B15,B16,B17,B18,B19,B20,B21,B22,B23,B24,B25]
+y = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+     1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+#X = [A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, B1, B2, B3, B4, B5, B6, B7, B8, B9, B10]
+#y = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+
+X = StandardScaler().fit_transform(X)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=True)
+
+#test dataset
+M1 = [1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1]
+M2 = [1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1]
+M3 = [0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1]
+M4 = [1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0]
+M5 = [0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1]
+Mystery = [M1, M2, M3, M4, M5]
+# models
+model1 = MLPClassifier(activation='relu', alpha=1e-05, batch_size='auto',
+              beta_1=0.9, beta_2=0.999, early_stopping=False,
+              epsilon=1e-08, hidden_layer_sizes=(5, 2),
+              learning_rate='constant', learning_rate_init=0.001,
+              max_iter=200, momentum=0.9,
+              nesterovs_momentum=True, power_t=0.5, random_state=1,
+              shuffle=True, solver='lbfgs', tol=0.0001,
+              validation_fraction=0.1, verbose=False, warm_start=False)
+model1.fit(X_train, y_train)
+result1 = model1.predict(Mystery)
+result11 = []
+for i in result1:
+    if i == 1:
+        result11.append('ClassB')
+    elif i == 0:
+        result11.append('ClassA')
+print('predict results of MLPClassifier: ',result11,' cross validation scores: ',cross_val_score(model1, X_test, y_test, scoring='mean_squared_error',cv=5))
+
+
+
+model2 = RandomForestClassifier(n_estimators=30,min_samples_leaf=4,max_features=11)
+model2.fit(X_train, y_train)
+result2 = model1.predict(Mystery)
+result22 = []
+for i in result2:
+    if i == 1:
+        result22.append('ClassB')
+    elif i == 0:
+        result22.append('ClassA')
+print('predict results of RandomForestClassifier: ', result11, ' cross validation scores: ', cross_val_score(model1, X_test, y_test, scoring='mean_squared_error', cv=5))
+
+
+# validation curve
+'''
+param_range = [i+1 for i in range(25)]
+print(param_range)
+train_score, test_score = validation_curve(RandomForestClassifier(n_estimators=30,min_samples_leaf=4),
+                                           X,y,
+                                           param_name='max_features',
+                                           param_range=param_range,
+                                           cv=None,scoring='mean_squared_error')
+train_score = -np.mean(train_score,axis=1)
+test_score = -np.mean(test_score,axis=1)
+plt.plot(param_range,train_score,'o-',color = 'r',label = 'training')
+plt.plot(param_range,test_score,'o-',color = 'g',label = 'testing')
+plt.legend(loc='best')
+plt.xlabel('max_features')
+plt.ylabel('mean_squared_error')
+plt.show()
+
+'''
