2c23dd946a79ee60e9f674dc9465b5de1b880ed1
FreddieSun <freddiesun95@gmail.com>
2018-12-20 00:50:59 -0500

    Finished

--- Final/Q2/mdp_ab.py
+++ Final/Q2/mdp_ab.py
@@ -1,61 +1,48 @@
-
-
 cost_replace = 250
 beta = 0.9
 epsilon = 0.001
-epochs = 0
 optimal_policy_list = []
 action_choice = ['USE', 'REPLACE']
 
-
-# init the utility for each state
 utility_list = []
+trans_probability = []
+remain_probability = []
+replace_probability = []
+reward = []
+
 for i in range(10):
+    # init the utility for each state
     utility_list.append(0)
 
-# init the transfor probability for each state
-trans_probability = []
-for i in range(10):
-    if i == 0:
-        trans_probability.append(1)
-        continue
+    # init the transfor probability for each state
     trans_probability.append(0.1 * i)
 
-# init the remain probability for each state
-remain_probability = []
-
-for i in range(10):
-    remain_probability.append(1 - trans_probability[i])
-
-
-# init the replace probability for each state
-replace_probability = []
-for i in range(10):
-    if i == 0:
-        replace_probability.append(0)
-        continue
+    # init the replace probability for each state
     replace_probability.append(1)
 
-
-# init the reward
-reward = []
-for i in range(10):
+    # init the reward
     reward.append(100 - i * 10)
 
-reward[-1] = 0
+trans_probability[0] = 1
+replace_probability[0] = 0
 
+for i in range(10):
+    # init the remain probability for each state
+    remain_probability.append(1 - trans_probability[i])
+
+reward[-1] = 0
 
 while (True):
     new_utility = []
     actions = []
     delta = 0
-    epochs += 1
 
     for i in range(9):
         temp_value = [(remain_probability[i] * (reward[i] + beta * utility_list[i]) +
                        trans_probability[i] * (reward[i] + beta * utility_list[i + 1])),
                       replace_probability[i] * (-cost_replace + beta * utility_list[0])]
         utility_updated = max(temp_value)
+
         actions.append(temp_value.index(utility_updated))
         new_utility.append(utility_updated)
         if abs(utility_updated - utility_list[i]) > delta:
@@ -74,6 +61,10 @@ while (True):
         break
 
 optimal_policy_list = [action_choice[i] for i in optimal_policy_list]
+
+print("The utility for each state is:")
 print(utility_list)
+print()
+
+print("The optimal policy for each state is:")
 print(optimal_policy_list)
-print('epochs = ', epochs)
